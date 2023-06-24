818bf7598babe4a5bbc6e83e550eaadd31c76761
TheBigBryce <bclukens@wpi.edu>
2023-06-24 14:49:22 -0400

    everything renders

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 let gl;
 let vBufferSign,vNormalSign,vPositionSign,vNormalPositionSign, pointsArraySign = [], normalsArraySign = [];
 let vBufferCar,vNormalCar,vPositionCar,vNormalPositionCar, pointsArrayCar = [], normalsArrayCar = [];
+let vBufferLamp,vNormalLamp,vPositionLamp,vNormalPositionLamp, pointsArrayLamp = [], normalsArrayLamp = [];
+let vBufferBunnicula,vNormalBunnicula,vPositionBunnicula,vNormalPositionBunnicula, pointsArrayBunnicula = [], normalsArrayBunnicula = [];
+let vBufferStreet,vNormalStreet,vPositionStreet,vNormalPositionStreet, pointsArrayStreet = [], normalsArrayStreet = [];
 
+
+let ctMatrixLoc;
 let eye;
-var at = vec3(3.0, -2.0, 0.0);
+var at = vec3(3.0, 0.0, 0.0);
 var up = vec3(0.0, 1.0, 0.0);
 let modelViewMatrix;
 let projectionMatrix;
@@ -62,7 +67,7 @@ function main() {
 
 
 
-    eye = vec3(10,10,10);
+    eye = vec3(10,30,10);
     modelViewMatrix = lookAt(eye, at , up);
     var fovy = 30;
     projectionMatrix = perspective(fovy,1,near,far);
@@ -71,13 +76,68 @@ function main() {
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
 
+    ctMatrixLoc = gl.getUniformLocation(program, "translate");
+
     vPositionSign = gl.getAttribLocation( program, "vPositionSign");
     vNormalPositionSign = gl.getAttribLocation( program, "vNormalSign");
     vPositionCar = gl.getAttribLocation( program, "vPositionCar");
     vNormalPositionCar = gl.getAttribLocation( program, "vNormalCar");
-
+    vPositionStreet = gl.getAttribLocation( program, "vPositionStreet");
+    vNormalPositionStreet = gl.getAttribLocation( program, "vNormalStreet");
+    vPositionBunnicula = gl.getAttribLocation( program, "vPositionBunnicula");
+    vNormalPositionBunnicula = gl.getAttribLocation( program, "vNormalBunnicula");
+    vPositionLamp = gl.getAttribLocation( program, "vPositionLamp");
+    vNormalPositionLamp = gl.getAttribLocation( program, "vNormalLamp");
 
    render();
+}
+
+
+var id;
+function render(){
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.uniformMatrix4fv(ctMatrixLoc, false, flatten(translate(1,1,1)));
+    gl.uniform1f(gl.getUniformLocation(program,"drawFlag"),5.0);
+    gl.uniform1f(gl.getUniformLocation(program,"drawFlag2"),5.0);
+    if(stopSign.objParsed && stopSign.mtlParsed && !stopSign.loaded){
+        loadStopSign(stopSign);
+        stopSign.loaded=true;
+    }
+    gl.drawArrays(gl.TRIANGLES, 0, pointsArraySign.length);
+
+    gl.uniform1f(gl.getUniformLocation(program,"drawFlag"),4.0);
+    gl.uniform1f(gl.getUniformLocation(program,"drawFlag2"),4.0);
+    if(car.objParsed && car.mtlParsed && !car.loaded){
+        loadCar(car);
+        car.loaded=true;
+    }
+    gl.drawArrays(gl.TRIANGLES, 0, pointsArrayCar.length);
+
+    gl.uniform1f(gl.getUniformLocation(program,"drawFlag"),3.0);
+    gl.uniform1f(gl.getUniformLocation(program,"drawFlag2"),3.0);
+    if(lamp.objParsed && lamp.mtlParsed && !lamp.loaded){
+        loadLamp(lamp);
+        lamp.loaded=true;
+    }
+    gl.drawArrays(gl.TRIANGLES, 0, pointsArrayLamp.length);
+
+    gl.uniform1f(gl.getUniformLocation(program,"drawFlag"),2.0);
+    gl.uniform1f(gl.getUniformLocation(program,"drawFlag2"),2.0);
+    if(street.objParsed && street.mtlParsed && !street.loaded){
+        loadStreet(street);
+        street.loaded=true;
+    }
+    gl.drawArrays(gl.TRIANGLES, 0, pointsArrayStreet.length);
+
+    gl.uniform1f(gl.getUniformLocation(program,"drawFlag"),1.0);
+    gl.uniform1f(gl.getUniformLocation(program,"drawFlag2"),1.0);
+    if(bunnicula.objParsed && bunnicula.mtlParsed && !bunnicula.loaded){
+        loadBunnicula(bunnicula);
+        bunnicula.loaded=true;
+    }
+    gl.drawArrays(gl.TRIANGLES, 0, pointsArrayBunnicula.length);
+
+    id = requestAnimationFrame(render);
 }
 
 
@@ -104,28 +164,6 @@ function loadStopSign(curObj){
     gl.enableVertexAttribArray(vNormalPositionSign);
 }
 
-var id;
-function render(){
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.uniform1f(gl.getUniformLocation(program,"drawFlag"),5.0);
-    gl.uniform1f(gl.getUniformLocation(program,"drawFlag2"),5.0);
-    if(stopSign.objParsed && stopSign.mtlParsed && !stopSign.loaded){
-        loadStopSign(stopSign);
-        stopSign.loaded=true;
-    }
-    gl.drawArrays(gl.TRIANGLES, 0, pointsArraySign.length);
-
-    gl.uniform1f(gl.getUniformLocation(program,"drawFlag"),4.0);
-    gl.uniform1f(gl.getUniformLocation(program,"drawFlag2"),4.0);
-    if(car.objParsed && car.mtlParsed && !car.loaded){
-        loadCar(car);
-        car.loaded=true;
-    }
-    gl.drawArrays(gl.TRIANGLES, 0, pointsArrayCar.length);
-    id = requestAnimationFrame(render);
-}
-
-
 function loadCar(curObj){
     for(let i=0; i<curObj.faces.length; i++){
         let curFace = curObj.faces[i];
@@ -135,8 +173,6 @@ function loadCar(curObj){
         }
     }
 
-    console.log(pointsArrayCar[2])
-    console.log(pointsArrayCar.length);
     vBufferCar = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBufferCar);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArrayCar), gl.STATIC_DRAW);
@@ -150,4 +186,75 @@ function loadCar(curObj){
 
     gl.vertexAttribPointer(vNormalPositionCar, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vNormalPositionCar);
+}
+function loadLamp(curObj){
+    for(let i=0; i<curObj.faces.length; i++){
+        let curFace = curObj.faces[i];
+        for(let j=0; j<curFace.faceVertices.length; j++) {
+            pointsArrayLamp.push(curFace.faceVertices[j]);
+            normalsArrayLamp.push(curFace.faceNormals[j]);
+        }
+    }
+
+    vBufferLamp = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferLamp);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArrayLamp), gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(vPositionLamp, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPositionLamp);
+
+    vNormalLamp = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vNormalLamp);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArrayLamp), gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(vNormalPositionLamp, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormalPositionLamp);
+}
+
+function loadStreet(curObj){
+    for(let i=0; i<curObj.faces.length; i++){
+        let curFace = curObj.faces[i];
+        for(let j=0; j<curFace.faceVertices.length; j++) {
+            pointsArrayStreet.push(curFace.faceVertices[j]);
+            normalsArrayStreet.push(curFace.faceNormals[j]);
+        }
+    }
+
+    vBufferStreet = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferStreet);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArrayStreet), gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(vPositionStreet, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPositionStreet);
+
+    vNormalStreet = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vNormalStreet);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArrayStreet), gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(vNormalPositionStreet, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormalPositionStreet);
+}
+
+function loadBunnicula(curObj){
+    for(let i=0; i<curObj.faces.length; i++){
+        let curFace = curObj.faces[i];
+        for(let j=0; j<curFace.faceVertices.length; j++) {
+            pointsArrayBunnicula.push(curFace.faceVertices[j]);
+            normalsArrayBunnicula.push(curFace.faceNormals[j]);
+        }
+    }
+
+    vBufferBunnicula = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferBunnicula);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArrayBunnicula), gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(vPositionBunnicula, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPositionBunnicula);
+
+    vNormalBunnicula = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vNormalBunnicula);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArrayBunnicula), gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(vNormalPositionBunnicula, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormalPositionBunnicula);
 }
