5c99c2e6ebca8236c2d49733da370b1662a96282
TheBigBryce <bclukens@wpi.edu>
2023-06-23 21:10:02 -0400

    stop sign wont render

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let gl;
 let vPosition;
 let eye;
 var at = vec3(0.0, 0.0, 0.0);
-var up = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, 1.0, 0.0);
 let modelViewMatrix;
 let projectionMatrix;
 let modelViewMatrixLoc, projectionMatrixLoc;
@@ -13,6 +13,31 @@ var far = 100;
 let program;
 let pointsArray, normalsArray;
 let vNormalPosition;
+
+// Get the stop sign
+let stopSign = new Model(
+    "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.obj",
+    "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.mtl");
+
+// Get the lamp
+let lamp = new Model(
+    "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/lamp.obj",
+    "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/lamp.mtl");
+
+// Get the car
+let car = new Model(
+    "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/car.obj",
+    "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/car.mtl");
+
+// Get the street
+let street = new Model(
+    "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/street.obj",
+    "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/street.mtl");
+
+// Get the bunny (you will not need this one until Part II)
+let bunny = new Model(
+    "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/bunny.obj",
+    "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/bunny.mtl");
 function main() {
     // Retrieve <canvas> element
     let canvas = document.getElementById('webgl');
@@ -32,37 +57,14 @@ function main() {
     // Set clear color
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
+    gl.enable(gl.DEPTH_TEST);
     // Initialize shaders
     program = initShaders(gl, "vshader", "fshader");
     gl.useProgram(program);
 
-    // Get the stop sign
-    let stopSign = new Model(
-        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.obj",
-        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.mtl");
-
-    // Get the lamp
-    let lamp = new Model(
-        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/lamp.obj",
-        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/lamp.mtl");
-
-    // Get the car
-    let car = new Model(
-        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/car.obj",
-        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/car.mtl");
-
-   // Get the street
-    let street = new Model(
-        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/street.obj",
-        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/street.mtl");
-
-    // Get the bunny (you will not need this one until Part II)
-    let bunny = new Model(
-        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/bunny.obj",
-        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/bunny.mtl");
 
 
-    eye = vec3(0,0,0);
+    eye = vec3(10,10,40);
     modelViewMatrix = lookAt(eye, at , up);
     var fovy = 30;
     projectionMatrix = perspective(fovy,1,near,far);
@@ -73,15 +75,25 @@ function main() {
 
     vPosition = gl.getAttribLocation( program, "vPosition");
     vNormalPosition = gl.getAttribLocation( program, "vNormal");
-    loadObj(stopSign);
+
+    pointsArray = [];
+    normalsArray = [];
+
+
+   render(stopSign);
 }
 
 
 function loadObj(curObj){
     pointsArray = [];
     normalsArray = [];
-    //putInArrays(curObj);
-
+    for(let i=0; i<curObj.faces.length; i++){
+        let curFace = curObj.faces[i];
+        for(let j=0; j<curFace.faceVertices.length; j++) {
+            pointsArray.push(curFace.faceVertices[j]);
+            normalsArray.push(curFace.faceNormals[j]);
+        }
+    }
     vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
@@ -95,13 +107,16 @@ function loadObj(curObj){
 
     gl.vertexAttribPointer(vNormalPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vNormalPosition);
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.drawArrays(gl.LINES, 0, pointsArray.length);
 }
-function putInArrays(curObj){
-    for(let i=0; i<curObj.faces.length; i++){
-       let curFaces=curObj.faces;
-        pointsArray.push(curFaces[i].faceVertices);
-        normalsArray.push(curFaces[i].faceNormals);
+
+var id;
+function render(){
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    if(stopSign.objParsed && stopSign.mtlParsed && !stopSign.loaded){
+        loadObj(stopSign);
+        stopSign.loaded=true;
     }
+
+    gl.drawArrays(gl.POINTS, 0, pointsArray.length);
+        id = requestAnimationFrame(render);
 }
